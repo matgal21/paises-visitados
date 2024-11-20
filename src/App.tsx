570ec2d45195d0bbe0ace233819cd74772db4f65
Matgal <chinogalvez5@gmail.com>
2024-11-20 18:38:14 -0300

    logo

--- src/App.tsx
+++ src/App.tsx
@@ -28,8 +28,10 @@ function App() {
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="flex justify-between h-16 items-center">
             <div className="flex items-center">
+              <a href="/" className='flex items-center'>
               <Plane className="h-8 w-8 text-indigo-600" />
-              <span className="ml-2 text-xl font-bold text-gray-800">Países visitados</span>
+              <span className="ml-2 text-xl font-bold text-gray-800 ">Países visitados</span>
+              </a>
             </div>
             <button
               onClick={() => setShowList(!showList)}
